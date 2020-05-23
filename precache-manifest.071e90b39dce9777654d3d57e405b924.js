f9c2d5429fe2c62ad47281d6c0c5757a1729987a
Flare-k <rokkyw@naver.com>
2020-05-23 21:26:06 +0900

    Updates

---
+++ precache-manifest.071e90b39dce9777654d3d57e405b924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a00ba62ff623874274f9c624cb6a54",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d3db088e72957375031c",
+    "url": "/movie_app_2020/static/css/main.3875fb73.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3db088e72957375031c",
+    "url": "/movie_app_2020/static/js/main.93f95d8c.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
